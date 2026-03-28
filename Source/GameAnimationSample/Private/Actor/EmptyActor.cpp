5af20a98dd9735e6f1758ecf078f12abec313200
MuirV <370941566@qq.com>
2026-03-28 21:01:34 +0800

    fix: None class lead to Editor can't scan the c++ floder

---
+++ Source/GameAnimationSample/Private/Actor/EmptyActor.cpp
@@ -0,0 +1,27 @@
+// Copyright Daisy
+
+
+#include "Actor/EmptyActor.h"
+
+// Sets default values
+AEmptyActor::AEmptyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEmptyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEmptyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
